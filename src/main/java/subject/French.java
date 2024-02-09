83aed3d0e4630cb261b3c811718c497bcdbef62e
Meghnatangallapalli <meghana.tangallapall>
2024-02-09 16:09:32 +0530

    F

---
+++ src/main/java/subject/French.java
@@ -0,0 +1,12 @@
+package subject;
+
+public class French {
+
+	public static void main(String[] args) {
+		
+	String url = "jdbc:postgresql://localhost/test";
+	String name = "postgres";
+	String password = "1234";
+	}
+
+}
---
+++ src/test/java/com/session/mypkg/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.session.mypkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
